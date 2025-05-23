5ba4e7e91a8e66b08af589c5460e39ee939767b9
mustakim67-coder <mustakimapple95@gmail.com>
2025-05-23 12:41:48 +0600

    Added delete feature to delete group from ui and database

--- src/Components/MyGroups.jsx
+++ src/Components/MyGroups.jsx
@@ -1,17 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useLoaderData } from 'react-router';
 import { AuthContext } from '../Context/AuthContext';
 import { MdEdit, MdDelete, MdOutlineManageAccounts } from "react-icons/md";
+import Swal from 'sweetalert2';
 
 const MyGroups = () => {
     const { user } = useContext(AuthContext);
-    const data = useLoaderData();
-    const result = data?.filter(group => group.userEmail === user.email) || [];
+    const groups = useLoaderData();
+    const [data, setData] = useState(groups)
+    const result = data.filter(group => group.userEmail === user.email) || "";
+
+    const handleDelete = (_id) => {
+        console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            console.log(result.isConfirmed)
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/groups/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(response => {
+                        if (response.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your group has been deleted.",
+                                icon: "success"
+                            });
+
+                            const remainingGroups = data.filter(group => group._id !== _id);
+                            setData(remainingGroups);
+                        }
+                    })
+
+            }
+        });
+    }
 
     return (
         <div className="px-4 md:px-[7%] py-8">
-            
-
             {result.length > 0 ? (
                 <div className="overflow-x-auto">
                     <h1 className="text-2xl font-bold text-center mb-10 underline">My Groups</h1>
@@ -47,16 +82,8 @@ const MyGroups = () => {
                                     <td className="px-4 py-3">{group.category}</td>
                                     <td className="px-4 py-3 text-center">
                                         <div className="flex flex-wrap gap-2 justify-center">
-                                            <Link>
-                                                <span className="btn btn-sm flex r gap-1">
-                                                    Update <MdEdit size={15} />
-                                                </span>
-                                            </Link>
-                                            <Link>
-                                                <span className="btn btn-sm flex gap-1">
-                                                    Delete <MdDelete size={15} />
-                                                </span>
-                                            </Link>
+                                            <Link><span className="btn btn-sm flex gap-1">Update <MdEdit size={15} /></span></Link>
+                                            <Link><span onClick={() => handleDelete(group._id)} className="btn btn-sm flex gap-1">Delete <MdDelete size={15} /></span></Link>
                                         </div>
                                     </td>
                                 </tr>
@@ -66,7 +93,7 @@ const MyGroups = () => {
                 </div>
             ) : (
                 <div className="text-center mt-10 text-gray-600 text-lg">
-                   <h1> No groups found !!</h1>
+                    <h1> No groups found !!</h1>
                 </div>
             )}
         </div>
