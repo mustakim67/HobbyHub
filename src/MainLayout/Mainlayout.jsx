64d97a325f45dc5d6348ebea516eda5174544cf6
mustakim67-coder <mustakimapple95@gmail.com>
2025-05-24 09:06:28 +0600

    Added Footer

--- src/MainLayout/Mainlayout.jsx
+++ src/MainLayout/Mainlayout.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import { Outlet } from 'react-router';
 import { ToastContainer } from 'react-toastify';
+import Footer from '../Components/Footer';
 
 const Mainlayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <div className='mt-15'>
+                <Footer></Footer>
+            </div>
+            
              <ToastContainer />
         </div>
     );
